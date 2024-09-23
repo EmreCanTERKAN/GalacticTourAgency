84f33cc3d6458856a982043f26cdcc57cd78c60e
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-09-23 16:35:36 +0300

    Nugetten EntityFramework, Tools  ve Migration kütüphanelerini ekleyip , Db contex yardımıyla veri tabanı bağlantısı yapmış olduk.

---
+++ GalacticTourAgency/Data/GalacticContext.cs
@@ -0,0 +1,18 @@
+﻿using GalacticTourAgency.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalacticTourAgency.Data
+{
+    // Dbcontex olduğunu belirtmek için öncelikle dbcontext sınıfından miras almaktır.
+    public class GalacticContext : DbContext
+    {
+        // Çoğul isim kullanılır.
+        public DbSet<GalacticProduct> GalacticProducts { get; set; }
+
+        // OnConfiguring metodunu ezerek bu database'in hangi databesi kullanacağını belirtebiliriz.
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Galactic;Truested_Connection=true");
+        //}
+    }
+}
